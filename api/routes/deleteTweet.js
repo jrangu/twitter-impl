ea1757ac5480a4840b45b3f5d94087015f5283dd
Jahnavi Rangu <55324369+jrangu@users.noreply.github.com>
2019-10-20 04:06:59 -0700

    implemented delete tweet

--- api/routes/deleteTweet.js
+++ api/routes/deleteTweet.js
@@ -5,10 +5,11 @@ var router = express.Router();
 var Twitter = require("twitter-node-client").Twitter;
 import {CONFIG} from "./constants"
 
-router.get("/", function(req, res, next) {
+router.post("/:id", function(req, res, next) {
     var twitter = new Twitter(CONFIG);
-    var options = { screen_name: 'JahnaviRangu'};
-    twitter.getUserTimeline(options, function(err, response, body) {
+    var url = 'https://api.twitter.com/1.1/statuses/destroy/'+req.params.id +'.json';
+    console.log(url);
+    twitter.doPost(url, {}, function(err, response, body) {
         console.log(err);
     }, function(data){
         console.log(data)
