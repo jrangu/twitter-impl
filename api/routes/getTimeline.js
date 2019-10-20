c767dab7e91a7a07a80543f777ee17867c862a2f
Jahnavi Rangu <55324369+jrangu@users.noreply.github.com>
2019-10-20 01:45:46 -0700

    implemented getting user timeline

--- api/routes/getTimeline.js
+++ api/routes/getTimeline.js
@@ -2,26 +2,23 @@ import 'dotenv/config';
 
 var express = require("express");
 var router = express.Router();
-var twitter = require("twit")
-
-// var T = new Twit(constants.credentials)
-
-// var options = { screen_name: 'sandagolcea',
-//                 count: 3 };
-
-// T.get('statuses/user_timeline', options , function(err, data) {
-//   for (var i = 0; i < data.length ; i++) {
-//     console.log(data[i].text);
-//   }
-// })
-
+var Twitter = require("twitter-node-client").Twitter;
 
 router.get("/", function(req, res, next) {
-    console.log(process.env.consumer_key)
-    console.log(process.env.consumer_secret)
-    console.log(process.env.access_token)
-    console.log(process.env.access_token_secret)
-    res.send("Hellp");
+    var config = {
+        consumerKey: process.env.consumer_key,
+        consumerSecret: process.env.consumer_secret,
+        accessToken: process.env.access_token,
+        accessTokenSecret: process.env.access_token_secret
+    }
+    var twitter = new Twitter(config)
+    var options = { screen_name: 'JahnaviRangu'};
+    twitter.getUserTimeline(options, function(err, response, body) {
+
+    }, function(data){
+        console.log(data)
+        res.send(data);
+    });
 });
 
 module.exports = router;
